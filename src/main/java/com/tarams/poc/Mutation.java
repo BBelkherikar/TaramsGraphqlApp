7f781f884d3eec92e990f9dc055c037a3cd87f27
Sreejith <sreejith_variyath@mheducation.com>
2019-12-02 13:43:05 +0530

    code changes to integrate with UI

--- src/main/java/com/tarams/poc/Mutation.java
+++ src/main/java/com/tarams/poc/Mutation.java
@@ -12,8 +12,17 @@ public class Mutation implements GraphQLMutationResolver {
 	@Autowired
 	private UserService userService;
 
-	public User createUser(String login, String name) {
-		return this.userService.newUser(login, name);
-	}
+//	public User createUser(String login, String name) {
+//		return this.userService.newUser(login, name);
+//	}
+	
+	
+	public User createUser(String userName, String password, String email, String firstName, String lastName, String phone, String city) {
+		return this.userService.newUser(userName, password, email, firstName, lastName, phone, city);
+		}
+	
+	public User updateUser(int id , String userName, String password, String email, String firstName, String lastName, String phone, String city) {
+		return this.userService.updateUser(id, userName, password, email, firstName, lastName, phone, city);
+		}
 
 }
--- src/main/java/com/tarams/poc/User.java
+++ src/main/java/com/tarams/poc/User.java
@@ -11,11 +11,19 @@ public class User {
 		@Id @GeneratedValue 
 		private int id ;
 		
-		// User-name (login id)
-		private String login ;
-		
-		// Real name of the user
-		private String name ;
+		private String userName;
+
+		private String password;
+
+		private String email;
+
+		private String firstName;
+
+		private String lastName;
+
+		private String phone;
+
+		private String city;
 
 		public int getId() {
 			return id;
@@ -25,26 +33,69 @@ public class User {
 			this.id = id;
 		}
 
-		public String getLogin() {
-			return login;
+		public String getUserName() {
+			return userName;
 		}
 
-		public void setLogin(String login) {
-			this.login = login;
+		public void setUserName(String userName) {
+			this.userName = userName;
 		}
 
-		public String getName() {
-			return name;
+		public String getPassword() {
+			return password;
 		}
 
-		public void setName(String name) {
-			this.name = name;
+		public void setPassword(String password) {
+			this.password = password;
+		}
+
+		public String getEmail() {
+			return email;
+		}
+
+		public void setEmail(String email) {
+			this.email = email;
+		}
+
+		public String getFirstName() {
+			return firstName;
+		}
+
+		public void setFirstName(String firstName) {
+			this.firstName = firstName;
+		}
+
+		public String getLastName() {
+			return lastName;
+		}
+
+		public void setLastName(String lastName) {
+			this.lastName = lastName;
+		}
+
+		public String getPhone() {
+			return phone;
+		}
+
+		public void setPhone(String phone) {
+			this.phone = phone;
+		}
+
+		public String getCity() {
+			return city;
+		}
+
+		public void setCity(String city) {
+			this.city = city;
 		}
 
 		@Override
 		public String toString() {
-			return "User [id=" + id + ", login=" + login + ", name=" + name + "]";
+			return "User [id=" + id + ", userName=" + userName + ", password=" + password + ", email=" + email
+					+ ", firstName=" + firstName + ", lastName=" + lastName + ", phone=" + phone + ", city=" + city
+					+ "]";
 		}
-		
 
+		
+		
 }
